02a9727355f3ecaf523de1048d6220380a00ca28
Miljan <miljancode@gmail.com>
2024-10-08 11:34:14 +0200

    chore(react): routes

--- apps/react/src/App.tsx
+++ apps/react/src/App.tsx
@@ -1,25 +1,63 @@
+import { Link, Outlet } from 'react-router-dom';
+
 export const App = () => {
   return (
     <div className="flex justify-center pt-4">
       <div className="max-w-xl w-full space-y-4">
         <div className="bg-slate-100 rounded-md flex items-center justify-between p-3">
           <span className="text-slate-700">You are not signed in.</span>
-          <button className="bg-blue-600 text-white py-1.5 px-4 rounded-md">
+          <Link
+            to="/signin"
+            className="bg-blue-600 text-white py-1.5 px-4 rounded-md"
+          >
             Sign in
-          </button>
+          </Link>
         </div>
 
         <ul className="flex items-center gap-2 [&>li]:text-slate-700 [&>li]:underline [&>li]:underline-offset-2">
-          <li>Home</li>
-          <li>Protected</li>
-          <li>Protected (API)</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected</Link>
+          </li>
         </ul>
 
-        <div className="space-y-1 pt-4">
-          <h1 className="text-2xl font-bold">React/Express/Auth.js Example</h1>
-          <p className="text-slate-500">
-            This is an example of how to use React with Express and Auth.js
-          </p>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export const Index = () => {
+  return (
+    <div className="space-y-1 pt-4">
+      <h1 className="text-2xl font-bold">React/Express/Auth.js Example</h1>
+      <p className="text-slate-500">
+        This is an example of how to use React with Express and Auth.js
+      </p>
+    </div>
+  );
+};
+
+export const Protected = () => {
+  return (
+    <div className="space-y-1 pt-4">
+      <h1 className="text-2xl font-bold">Protected route</h1>
+      <p className="text-slate-500">This route is protected. Session.</p>
+    </div>
+  );
+};
+
+export const SignIn = () => {
+  return (
+    <div className="flex justify-center pt-4">
+      <div className="max-w-xl w-full space-y-4">
+        <Link to="/" className="text-slate-500 text-sm">
+          &larr; Go back to Home page
+        </Link>
+        <div className="bg-slate-100 rounded-md flex items-center justify-between p-3">
+          Sign in
         </div>
       </div>
     </div>
--- apps/react/src/main.tsx
+++ apps/react/src/main.tsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './app';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { App, Index, Protected, SignIn } from './app';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Index />,
+      },
+      {
+        path: '/protected',
+        element: <Protected />,
+      },
+    ],
+  },
+  {
+    path: '/signin',
+    element: <SignIn />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
