2dcbafc5fd87a98b8c5b7ad663e67095e1b68f6b
Miljan <miljancode@gmail.com>
2024-10-09 17:34:05 +0200

    chore(ng): routes

--- apps/angular/src/app/app.component.ts
+++ apps/angular/src/app/app.component.ts
@@ -1,13 +1,91 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `<router-outlet />`,
 })
-export class AppComponent {
-  title = 'angular';
-}
+export class AppComponent {}
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  template: `
+    <header class="py-4 flex flex-col items-center [&>*]:max-w-xl [&>*]:w-full">
+      <div
+        class="bg-slate-200 rounded-md p-4 text-slate-700 flex items-center justify-between"
+      >
+        <span>You are not logged in</span>
+        <a
+          routerLink="/signin"
+          class="bg-blue-500 text-white py-1.5 px-3 rounded-md"
+          >Sign in</a
+        >
+      </div>
+      <nav
+        class="p-4 flex items-center gap-2 [&>*]:underline [&>*]:underline-offset-2"
+      >
+        <a routerLink="/" class="text-blue-500">Home</a>
+        <a routerLink="/protected" class="text-blue-500">Protected</a>
+      </nav>
+    </header>
+    <router-outlet />
+  `,
+})
+export class LayoutComponent {}
+
+@Component({
+  selector: 'app-index',
+  standalone: true,
+  template: `
+    <div class="flex justify-center">
+      <div class="max-w-xl w-full p-4 text-slate-700 flex flex-col gap-1">
+        <h1 class="text-2xl font-bold">Angular/Express/Auth.js Example</h1>
+        <span class="text-slate-500">
+          This is a simple example of how to use Angular with Express and
+          Auth.js.
+        </span>
+      </div>
+    </div>
+  `,
+})
+export class IndexComponent {}
+
+@Component({
+  selector: 'app-protected',
+  standalone: true,
+  template: `
+    <div class="flex justify-center">
+      <div class="max-w-xl w-full p-4 text-slate-700 flex flex-col gap-1">
+        <h1 class="text-2xl font-bold">Protected component</h1>
+        <span class="text-slate-500"> This is a protected component. </span>
+      </div>
+    </div>
+  `,
+})
+export class ProtectedComponent {}
+
+@Component({
+  selector: 'app-signin',
+  standalone: true,
+  imports: [RouterLink],
+  template: `
+    <div class="flex justify-center">
+      <div class="max-w-xl w-full p-4 text-slate-700 space-y-2">
+        <a routerLink="/" class="text-blue-500">&larr; Go back to Home page</a>
+        <div
+          class="bg-slate-200 max-w-xl w-full rounded-md p-4 text-slate-700 flex items-center justify-between"
+        >
+          <span
+            class="bg-blue-500 text-white py-1.5 px-3 rounded-md cursor-pointer"
+            >Sign in with Google</span
+          >
+        </div>
+      </div>
+    </div>
+  `,
+})
+export class SignInComponent {}
--- apps/angular/src/app/app.config.ts
+++ apps/angular/src/app/app.config.ts
@@ -1,8 +1,34 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideRouter([
+      {
+        path: '',
+        loadComponent: () =>
+          import('./app.component').then((c) => c.LayoutComponent),
+        children: [
+          {
+            path: '',
+            loadComponent: () =>
+              import('./app.component').then((c) => c.IndexComponent),
+          },
+          {
+            path: 'protected',
+            loadComponent: () =>
+              import('./app.component').then((c) => c.ProtectedComponent),
+          },
+        ],
+      },
+      {
+        path: 'signin',
+        loadComponent: () =>
+          import('./app.component').then((c) => c.SignInComponent),
+      },
+    ]),
+  ],
 };
